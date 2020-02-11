a9b0d587ee786576b206f0ed9ad1349e9e41d251
Shih <moureenwanjiku98@gmail.com>
2020-02-11 17:10:32 -0500

    TestNG project complete

---
+++ src/test/java/test/LoginTest.java
@@ -0,0 +1,22 @@
+package test;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class LoginTest {
+	
+	@Test
+	public void loginTest() {
+		
+		System.setProperty("webdriver.chrome.driver", "./driver/chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		
+		//COPY PASTE LOGIN TEST CASE AND RUN IT	
+		
+	}
+	
+	
+	
+	
+}
